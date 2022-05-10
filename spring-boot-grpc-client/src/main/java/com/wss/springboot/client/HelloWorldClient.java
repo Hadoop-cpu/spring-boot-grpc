e91d3fb2d83a352309b45176a7340b39459c6b2f
wangsaisai <hadoop_cpu@163.com>
2022-05-10 14:17:44 +0800

    修改服务

---
+++ spring-boot-grpc-client/src/main/java/com/wss/springboot/client/HelloWorldClient.java
@@ -0,0 +1,38 @@
+package com.wss.springboot.client;
+
+import com.wss.springboot.libserver.Greeting;
+import com.wss.springboot.libserver.HelloWorldServiceGrpc;
+import com.wss.springboot.libserver.Person;
+import io.grpc.ManagedChannel;
+import io.grpc.ManagedChannelBuilder;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
+
+import javax.annotation.PostConstruct;
+
+/**
+ * @ClassName: HelloWorldClient
+ * @Author: admin
+ * @Date: 2022/5/10
+ * @Time: 13:57
+ */
+@Slf4j
+@Component
+public class HelloWorldClient {
+    /**
+     * 阻塞stub
+     */
+    private HelloWorldServiceGrpc.HelloWorldServiceBlockingStub serviceBlockingStub;
+
+    @PostConstruct
+    public void init() {
+        ManagedChannel managedChannel = ManagedChannelBuilder.forAddress("localhost", 9898).usePlaintext().build();
+        serviceBlockingStub = HelloWorldServiceGrpc.newBlockingStub(managedChannel);
+    }
+
+    public String sayHello(String firstName, String lastName) {
+        Person person = Person.newBuilder().setFirstName(firstName).setLastName(lastName).build();
+        Greeting greeting = serviceBlockingStub.sayHello(person);
+        return greeting.getMessage();
+    }
+}
